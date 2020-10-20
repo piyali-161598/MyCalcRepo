ff572ae0965bd2a47011ed37a7ebcbf8300be503
piyali-161598 <44195803+piyali-161598@users.noreply.github.com>
2020-10-20 11:18:15 +0530

    Update Calc.java

--- Calc.java
+++ Calc.java
@@ -6,9 +6,13 @@ return a+b;
   public static int sub(int a, int b){
 return a-b;
 }
+  public static int prod(int a, int b){
+return a*b;
+}
 public static void main(String[] args){
 System.out.println(add(10,20));
   System.out.println(sub(10,20));
+  System.out.println(prod(10,20));
 
 }
 }

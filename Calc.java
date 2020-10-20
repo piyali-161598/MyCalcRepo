06cace298ab77b287387b556d92ac08ed3780570
piyali-161598 <44195803+piyali-161598@users.noreply.github.com>
2020-10-20 10:56:30 +0530

    Update Calc.java

--- Calc.java
+++ Calc.java
@@ -3,7 +3,12 @@ class Calc{
 public static int add(int a, int b){
 return a+b;
 }
+  public static int sub(int a, int b){
+return a-b;
+}
 public static void main(String[] args){
 System.out.println(add(10,20));
+  System.out.println(sub(10,20));
+
 }
 }
